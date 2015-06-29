fd569c7b75eb333526239d1635bb78d935d9336d
yinjun920714 <junyin@umich.edu>
2015-06-29 11:49:48 -0400

    naive seperate cin and calculate

--- example2.cpp
+++ example2.cpp
@@ -1,3 +1,6 @@
+// command   ./a.out input_file M N ref_num 
+
+
 #include <iostream>
 #include <fstream>
 #include <cstring>
@@ -6,41 +9,57 @@
 #include <sys/time.h>
 #include <unistd.h>
 #define B 3
-#define L 10000
-//#define NUM_INDIVIDUAL 2184
+//#define N 10000
+//#define M 2184
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
+	ifstream in (argv[1]);
+	int M = atoi(argv[2]);
+	int N = atoi(argv[3]);
+	int ref_num = atoi(argv[4]);
+//	ofstream out ("test_output.txt");
+	int **data = new int*[N];
+	for (int i = 0; i < N; i++)
+		data[i] = new int[M];
+	int *prev_record = new int[M];      //record the consistent haplotypes labels of previous segment
+	int end;
+	int tmp = 0;
+    //calculate the ref seq. and 1 position
+    int *x;
+    int *pos; /* for the 1 position for ref */
+    int seg_num = 1; /* for the segment number  */
+    int num_1 = 0; /* for the num of 1 */
+    int k;  /* for the k th  site */
+	//input data
+	for ( int i = 0; i < N; i++ )
+		for ( int j = 0; j < M; j++ )
+			in>>data[i][j];
+
 	struct timeval tstart, tend;
     gettimeofday( &tstart, NULL );
-	
 
-	ifstream in (argv[1]);
-	int NUM_INDIVIDUAL = atoi(argv[2]);
-//	ofstream out ("test_output.txt");
-	int **data = new int*[L];
-	for (int i = 0; i < L; i++)
-		data[i] = new int[NUM_INDIVIDUAL];
-	int *prev_record = new int[NUM_INDIVIDUAL];      //record the consistent haplotypes labels of previous segment
-	int end = 0;
-	int ref[L];
-	int tmp = 0;
+
+    x = new int[N];
+    pos = new int[N];
+
+    for ( int i = 0; i < N; ++i)
+    	x[i] = data[i][ref_num * 2] + data[i][ref_num * 2 + 1];
+
+    for (int i = 0, j = 0; i < N; ++i) {
+    	if (x[i] == 1) {
+    		++j;
+    		pos[num_1++] = i;
+    		if (j == 3) {
+    			++seg_num;
+    			j = 0;
+    		}
+    	}
+    }
+
+
 	//first segment
-	for ( int i = 0; i < L; i++) {
-		for ( int j = 0 ; j < NUM_INDIVIDUAL; j++) {
-			in>>data[i][j];
-		}
-		ref[i] = data[i][0] + data[i][1];
-		if (ref[i] == 1) {
-			tmp++;	
-			if (tmp == B) {
-			end = i + 1;
-			break;
-			}				
-		}
-	}
-
 	int count[64];
 	for ( int i = 0; i < 64; i++)
 		count[i] = 0;
@@ -48,18 +67,18 @@ int main(int argc, char *argv[]) {
 	int record_idx;
 	bool consistent_flag;
 	//	compare
-	for ( int j = 0; j < NUM_INDIVIDUAL; j++) {
+	for ( int j = 0; j < M; j++) {
 		consistent_flag = true;
 		record_idx = 0;
-		for ( int i = 0; i < end; i++) {
-			if (ref[i] != 1) {
-				if (data[i][j] != ref[i] / 2) {
+		for ( k = 0; k <= pos[2]; k++) {
+			if (x[k] != 1) {
+				if (data[k][j] != x[k] / 2) {
 					consistent_flag = false;
 					prev_record[j] = 8;      // for non-consistent haplotypes
 					break;
 				}
 			} else {
-				record[record_idx++] = data[i][j];
+				record[record_idx++] = data[k][j];
 		    }
 		}
 		if (consistent_flag) {
@@ -78,54 +97,27 @@ int main(int argc, char *argv[]) {
 	cout<<"110 "<<count[6]<<endl;
 	cout<<"111 "<<count[7]<<endl;
 
-	bool last_segment = false;
-	int seg_num = 2;
+	int s = 2;
 	while (true) {
 		//from the second segment   
-		tmp = 0;
 		for ( int i = 0; i < 64; i++)
 			count[i] = 0;
-		for ( int i = 0; i < L; i++) {
-			for ( int j = 0 ; j < NUM_INDIVIDUAL; j++) {
-				if( !(in>>data[i][j])) {
-					last_segment = true;
-					break;				
-				}
-			}
-			if (last_segment) {
-				end = i;				
-				break;
-			}
-			ref[i] = data[i][0] + data[i][1];
-			if (ref[i] == 1) {
-				tmp++;	
-				if (tmp == B) {
-				end = i + 1;
-				break;
-				}				
-			}
-			if (i == L - 1) {
-				cout<<"exceed max length of segment"<<endl;
-				return -1;							
-			}
-		}
-		
-		if (last_segment) {
-			break;		
-		}
-		//	compare
-		for ( int j = 0; j < NUM_INDIVIDUAL; j++) {
+		if ( s > seg_num - 1 )   //last segment 
+			break;    
+		end = pos[s * 3 - 1];
+		for ( int j = 0; j < M; j++) {
+			k = pos[s * 3 -4] + 1;
 			consistent_flag = true;
 			record_idx = 0;
-			for ( int i = 0; i < end; i++) {
-				if (ref[i] != 1) {
-					if (data[i][j] != ref[i] / 2) {
+			for ( ; k <= end; k++) {
+				if (x[k] != 1) {
+					if (data[k][j] != x[k] / 2) {
 						consistent_flag = false;
 						prev_record[j] = 8;      // for non-consistent haplotypes
 						break;
 					}
 				} else {
-					record[record_idx++] = data[i][j];
+					record[record_idx++] = data[k][j];
 			    }
 			}
 			if (consistent_flag) {
@@ -137,7 +129,7 @@ int main(int argc, char *argv[]) {
 			}
 		}
 		
-		cout<<"\n\nsegment num\t"<<setw(4)<<seg_num++<<endl;
+		cout<<"\n\nsegment num\t"<<setw(4)<<s++<<endl;
 		cout<<setw(5)<<"index";
 		for (int j = 0; j < 8; j++)
 			cout<<setw(10)<<j;
@@ -152,55 +144,60 @@ int main(int argc, char *argv[]) {
 	
 	//compare last segment
 	int count_idx;
-	if (!tmp) {
-		for ( int j = 0; j < NUM_INDIVIDUAL; j++) {
+	tmp = num_1 - (s - 1) * 3;
+	if (tmp > 0) {
+		for ( int j = 0; j < M; j++) {
 			consistent_flag = true;
 			record_idx = 0;
-			for ( int i = 0; i < end; i++) {
-				if (ref[i] != 1) {
-					if (data[i][j] != ref[i] / 2) {
+			k = pos[(seg_num - 1)* 3 - 1] + 1;
+			for ( ; k < N; k++) {
+				if (x[k] != 1) {
+					if (data[k][j] != x[k] / 2) {
 						consistent_flag = false;
 						prev_record[j] = 8;      // for non-consistent haplotypes
 						break;
 					}
 				} else {
-					record[record_idx++] = data[i][j];
+					record[record_idx++] = data[k][j];
 			    }
 			}
 			if (consistent_flag) {
 				if (prev_record[j] != 8) {
 					count_idx = record[0];					
-					for (int k = 1; k < tmp; k++) {
-						count_idx = count_idx * 2 + record[k];
+					for (int i = 1; i < tmp; i++) {
+						count_idx = count_idx * 2 + record[i];
 					}
 					count[prev_record[j] * 8 + count_idx]++;
 				}			
 			}
 		}
 
-		for (int i = 0; i < tmp * 8; i++)
-			cout<<"index "<<i<<"     "<<count[i]<<endl;
+		int row = tmp == 1 ? 2 : 4;
+		cout<<"\n\nsegment num\t"<<setw(4)<<s++<<endl;
+		cout<<setw(5)<<"index";
+		for (int j = 0; j < row; j++)
+			cout<<setw(10)<<j;
+		cout<<endl;
+		for (int i = 0; i < 8; i++) {
+			cout<<setw(5)<<i;
+			for (int j = 0; j < row; j++)
+				cout<<setw(10)<<count[i * row + j];
+			cout<<endl;
+		}
 	}
 	in.close();
 //	out.close();
 
 	delete [] prev_record;
-	for (int i = 0; i < L; i++) {
+	for (int i = 0; i < N; i++) {
 		delete [] data[i];
 	}
 	delete [] data;
+	delete [] pos;
+	delete [] x;
 
 	gettimeofday( &tend, NULL );
     int timeuse = 1000000 * ( tend.tv_sec - tstart.tv_sec ) + tend.tv_usec -tstart.tv_usec;
-
-    
-    //calculate the input time;
-    gettimeofday( &tstart, NULL );
-    ifstream in2 ("phase1.chr20.10-11Mb.txt");
-    while(!(in2>>tmp));
-    in2.close();
-    gettimeofday( &tend, NULL );
-    tmp = 1000000 * ( tend.tv_sec - tstart.tv_sec ) + tend.tv_usec -tstart.tv_usec;
-	printf("time: %d us\n", timeuse - tmp);
+	printf("time: %d us\n", timeuse);
 	return 0;
 }
